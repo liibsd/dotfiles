ebbeacbd833e7a6946e54e2fb6120c57633fcb6b
void <>
2025-02-13 21:38:39 +0100

    Remove unused packages with xbps

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env sh
 test -d ~/dotfiles || exit 1
 cd ~/dotfiles
-which xbps-install >/dev/null 2>&1 && sudo xbps-install -Syu `cat .xbps`
-which doas >/dev/null 2>&1 && { test -e /etc/doas.conf || echo 'permit nopass :wheel' | doas tee /etc/doas.conf; }
+which xbps-install >/dev/null 2>&1 && {
+	sudo xbps-install -Syu `cat .xbps`
+	test -e /etc/xbps.d/00-ignorepkg-main.conf \
+		|| echo 'ignorepkg=openssh\nignorepkg=sudo\nignorepkg=nvi' | doas tee /etc/xbps.d/00-ignorepkg-main.conf
+}
+which doas >/dev/null 2>&1 && { test -e /etc/doas.conf || echo 'permit nopass :wheel' | sudo tee /etc/doas.conf; }
+which xbps-remove >/dev/null 2>&1 && {
+	which ssh >/dev/null 2>&1 && doas xbps-remove -y openssh
+	test -h /usr/bin/sudo || { which sudo >/dev/null 2>&1 && doas xbps-remove -y sudo; }
+	which nvi >/dev/null 2>&1 && doas xbps-remove -y nvi
+}
+test -h /usr/bin/sudo || doas ln -s /usr/bin/doas /usr/bin/sudo
 for cfg in `ls config`; do
 	test -d ~/.config/$cfg || mkdir ~/.config/$cfg
 	for file in `ls config/$cfg`; do
