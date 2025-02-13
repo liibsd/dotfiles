4887f38d3f88aa1c122fa0547d21d382f330de79
void <>
2025-02-13 20:56:38 +0100

    Add configurations

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,98 @@
+config.load_autoconfig(True)
+
+config.bind("y", "yank")
+
+config.bind("1", "tab-select 1")
+config.bind("2", "tab-select 2")
+config.bind("3", "tab-select 3")
+config.bind("4", "tab-select 4")
+config.bind("5", "tab-select 5")
+config.bind("6", "tab-select 6")
+config.bind("7", "tab-select 7")
+config.bind("8", "tab-select 8")
+config.bind("9", "tab-select 9")
+
+base00="#282828"
+base01="#3c3836"
+base02="#504945"
+base03="#665c54"
+base04="#bdae93"
+base05="#d5c4a1"
+base06="#ebdbb2"
+base07="#fbf1c7"
+base08="#fb4934"
+base09="#fe8019"
+base0A="#fabd2f"
+base0B="#b8bb26"
+base0C="#8ec07c"
+base0D="#83a598"
+base0E="#d3869b"
+base0F="#d65d0e"
+
+c.colors.completion.category.bg=base00
+c.colors.completion.category.border.bottom=base00
+c.colors.completion.category.border.top=base00
+c.colors.completion.category.fg=base0D
+c.colors.completion.even.bg=base00
+c.colors.completion.fg=base05
+c.colors.completion.item.selected.bg=base02
+c.colors.completion.item.selected.border.bottom=base02
+c.colors.completion.item.selected.border.top=base02
+c.colors.completion.item.selected.fg=base05
+c.colors.completion.item.selected.match.fg=base05
+c.colors.completion.match.fg=base09
+c.colors.completion.odd.bg=base00
+c.colors.completion.scrollbar.bg=base00
+c.colors.completion.scrollbar.fg=base05
+c.colors.contextmenu.disabled.bg=base01
+c.colors.contextmenu.disabled.fg=base04
+c.colors.contextmenu.menu.bg=base00
+c.colors.contextmenu.menu.fg= base05
+c.colors.contextmenu.selected.bg=base02
+c.colors.contextmenu.selected.fg=base05
+c.colors.downloads.bar.bg=base00
+c.colors.downloads.error.fg=base08
+c.colors.downloads.start.bg=base0D
+c.colors.downloads.start.fg=base00
+c.colors.downloads.stop.bg=base0C
+c.colors.downloads.stop.fg=base00
+c.colors.hints.bg=base0A
+c.colors.hints.fg=base00
+c.colors.hints.match.fg=base05
+c.colors.keyhint.bg=base00
+c.colors.keyhint.fg=base05
+c.colors.keyhint.suffix.fg=base05
+c.colors.messages.error.bg=base08
+c.colors.messages.error.border=base08
+c.colors.messages.error.fg=base00
+c.colors.messages.info.bg=base00
+c.colors.messages.info.border=base00
+c.colors.messages.info.fg=base05
+c.colors.messages.warning.bg=base0E
+c.colors.messages.warning.border=base0E
+c.colors.messages.warning.fg=base00
+c.colors.prompts.bg=base00
+c.colors.prompts.border=base00
+c.colors.prompts.fg=base05
+c.colors.prompts.selected.bg=base02
+c.colors.prompts.selected.fg=base05
+c.colors.statusbar.command.bg=base00
+c.colors.statusbar.command.fg=base05
+c.colors.tabs.bar.bg=base00
+c.colors.tabs.even.bg=base00
+c.colors.tabs.even.fg=base05
+c.colors.tabs.odd.bg=base00
+c.colors.tabs.odd.fg=base05
+c.colors.tabs.pinned.even.bg=base0B
+c.colors.tabs.pinned.even.fg=base00
+c.colors.tabs.pinned.odd.bg=base0B
+c.colors.tabs.pinned.odd.fg=base00
+c.colors.tabs.pinned.selected.even.bg=base02
+c.colors.tabs.pinned.selected.even.fg=base05
+c.colors.tabs.pinned.selected.odd.bg=base02
+c.colors.tabs.pinned.selected.odd.fg=base05
+c.colors.tabs.selected.even.bg=base02
+c.colors.tabs.selected.even.fg=base05
+c.colors.tabs.selected.odd.bg=base02
+c.colors.tabs.selected.odd.fg=base05
+c.colors.webpage.bg=base00
