26c4a4e575ca37a2a5e36c51d033e88693ab0ee7
void <>
2025-02-13 21:04:53 +0100

    Automatic config symlinking

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 test -d ~/dotfiles || exit 1
+cd ~/dotfiles
 which doas >/dev/null 2>&1 && { test -e /etc/doas.conf || echo 'permit nopass :wheel' | doas tee /etc/doas.conf; }
-test -d ~/.config/qutebrowser || mkdir ~/.config/qutebrowser
-test -h ~/.config/qutebrowser/config.py || ln -s ~/dotfiles/config/qutebrowser/config.py ~/.config/qutebrowser/config.py
-test -h ~/.config/foot || ln -s ~/dotfiles/config/foot ~/.config/foot
-test -h ~/.config/kak || ln -s ~/dotfiles/config/kak ~/.config/kak
-test -h ~/.config/git || ln -s ~/dotfiles/config/git ~/.config/git
+for cfg in `ls config`; do
+	test -d /home/main/.config/$cfg || mkdir /home/main/.config/$cfg
+	for file in `ls config/$cfg`; do
+		test -h /home/main/.config/$cfg/$file || ln -s /home/main/dotfiles/config/$cfg/$file /home/main/.config/$cfg/$file
+	done
+done
