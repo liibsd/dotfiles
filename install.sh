9aa4ab37b56f3b96c4abba91fd39f6003a36aff8
void <>
2025-02-13 21:51:32 +0100

    Add exists helper function

--- install.sh
+++ install.sh
@@ -2,16 +2,21 @@
 test "$USER" = main || exit 1
 test -d ~/dotfiles || exit 2
 cd ~/dotfiles
-which xbps-install >/dev/null 2>&1 && {
+
+exists() {
+	which "$@" >/dev/null 2>&1 || return 1
+}
+
+exists xbps-install && {
 	sudo xbps-install -Syu `cat .xbps`
 	test -e /etc/xbps.d/00-ignorepkg-main.conf \
 		|| echo 'ignorepkg=openssh\nignorepkg=sudo\nignorepkg=nvi' | sudo tee /etc/xbps.d/00-ignorepkg-main.conf
 }
-which doas >/dev/null 2>&1 && { test -e /etc/doas.conf || echo 'permit nopass :wheel' | sudo tee /etc/doas.conf; }
-which xbps-remove >/dev/null 2>&1 && {
-	which ssh >/dev/null 2>&1 && doas xbps-remove -y openssh
-	test -h /usr/bin/sudo || { which sudo >/dev/null 2>&1 && doas xbps-remove -y sudo; }
-	which nvi >/dev/null 2>&1 && doas xbps-remove -y nvi
+exists doas && { test -e /etc/doas.conf || echo 'permit nopass :wheel' | sudo tee /etc/doas.conf; }
+exists xbps-remove && {
+	exists ssh && doas xbps-remove -y openssh
+	test -h /usr/bin/sudo || { exists sudo && doas xbps-remove -y sudo; }
+	exists nvi && doas xbps-remove -y nvi
 }
 test -h /usr/bin/sudo || doas ln -s /usr/bin/doas /usr/bin/sudo
 for cfg in `ls config`; do
