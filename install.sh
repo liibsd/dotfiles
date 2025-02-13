bb086106109d46e0109017fdafc85ffa45f67774
void <>
2025-02-13 21:07:00 +0100

    Use home variable

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ test -d ~/dotfiles || exit 1
 cd ~/dotfiles
 which doas >/dev/null 2>&1 && { test -e /etc/doas.conf || echo 'permit nopass :wheel' | doas tee /etc/doas.conf; }
 for cfg in `ls config`; do
-	test -d /home/main/.config/$cfg || mkdir /home/main/.config/$cfg
+	test -d ~/.config/$cfg || mkdir ~/.config/$cfg
 	for file in `ls config/$cfg`; do
-		test -h /home/main/.config/$cfg/$file || ln -s /home/main/dotfiles/config/$cfg/$file /home/main/.config/$cfg/$file
+		test -h ~/.config/$cfg/$file \
+			|| ln -s ~/dotfiles/config/$cfg/$file ~/.config/$cfg/$file
 	done
 done
