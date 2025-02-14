ae93568d09856696a3b7b3d5c77577e0e4b03d6b
void <>
2025-02-14 17:09:37 +0100

    Use sudo instead of doas

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -11,20 +11,6 @@ exists() {
 ## Install packages
 exists xbps-install && sudo xbps-install -Syu `cat bootstrap/xbps.txt`
 
-## Doas config
-exists doas && { test -e /etc/doas.conf || echo 'permit nopass :wheel' | sudo tee /etc/doas.conf; }
-
-## Remove packages
-exists xbps-remove && {
-	test -e /etc/xbps.d/00-ignorepkg-main.conf \
-		|| echo 'ignorepkg=openssh\nignorepkg=sudo' | sudo tee /etc/xbps.d/00-ignorepkg-main.conf
-	exists ssh && doas xbps-remove -y openssh
-	test -h /usr/bin/sudo || { exists sudo && doas xbps-remove -y sudo; }
-}
-
-## Link sudo to doas
-test -h /usr/bin/sudo || doas ln -s /usr/bin/doas /usr/bin/sudo
-
 ## Config
 for config in `ls config -I bashrc`; do
 	test -d ~/.config/$config || mkdir ~/.config/$config
@@ -40,9 +26,9 @@ test -h ~/.bashrc || ln -s ~/dotfiles/config/bashrc ~/.bashrc
 
 ## User services
 test -d /etc/sv/ && { test -d /etc/sv/runsvdir-main || {
-	doas mkdir /etc/sv/runsvdir-main
-	echo '#!/bin/sh\nexec chpst -u "main:`id -Gn main | tr " " ":"`" runsvdir /home/main/dotfiles/service' \
-		| doas tee /etc/sv/runsvdir-main/run
-	doas chmod +x /etc/sv/runsvdir-main/run
-	doas ln -s /etc/sv/runsvdir-main /var/service
+	sudo mkdir /etc/sv/runsvdir-main
+	echo '#!/bin/sh\nexec chpst -u "main:`id -Gn main | tr " " ":"`" runsvdir /home/main/.local/service' \
+		| sudo tee /etc/sv/runsvdir-main/run
+	sudo chmod +x /etc/sv/runsvdir-main/run
+	sudo ln -s /etc/sv/runsvdir-main /var/service
 }; }
